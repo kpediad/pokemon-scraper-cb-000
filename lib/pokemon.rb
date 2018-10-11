f4b5183ba83fab1883f0d5130395673e55baa838
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-10-11 06:27:20 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,7 @@ class Pokemon
     sel = db.prepare('SELECT * FROM pokemon WHERE pokemon.id = ?')
     result = sel.execute(id)
     #puts result
-    puts result[:id]
+    puts result.inspect
     puts result[1]
     puts result[2]
     pkmn = Pokemon.new(id: result[0], name: result[1], type: result[2], db: db) if !result
