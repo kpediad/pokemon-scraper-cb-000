f795f016761cc2c14bbce0084dfce972493c9fc5
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-10-11 06:11:19 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,11 @@ class Pokemon
   attr_reader :id, :name, :type, :db
 
   def initialize(id:, name:, type:, db:)
-
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    self
   end
 
   def self.save(name, type, db)
